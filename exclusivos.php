28042bc68fa80e744f3ebc6ca6fd1e7c2f36a9f6
Bxnju <andres.benjumea@upb.edu.co>
2022-07-08 01:25:48 -0500

    Barra de navegacion, slider y modulos V.1.0

---
+++ exclusivos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/exclusivos.css">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	<title>Exclusivos | Flores Camar SAS</title>
+</head>
+
+<header>
+	<div class="up">
+		<h3 class="number"><i class="fa-solid fa-phone"></i> (+57) 311-332-2312</h3>
+		<h1 class="logo">Floristeria Camar S.A.S</h1>
+		<span class="location"><i class="fa-solid fa-location-dot"></i> Cra 83 44-3 Medellin, Colombia.</span>
+	</div>
+	<div class="down">
+		<h3>Jugando con tus sentimientos...</h3>
+	<nav>
+		<a class="active__link" href="index.html">Inicio</a>
+		<a href="catalogo.php">Catalogo</a>
+		<a href="exclusivos.php">Exclusivos</a>
+		<a href="contactanos.html">Contactanos</a>
+		<a href="nosotros.html">Sobre nosotros</a>
+	</nav>
+	</div>
+</header>
+
+<body>
+
+
+
+
+
+</body>
+</html>
