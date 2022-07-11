93c87161adf10d3641a47f348325e332e4893da9
Bxnju <andres.benjumea@upb.edu.co>
2022-07-11 04:10:34 -0500

    Navbar con efecto de Scroll e Imagenes del Logo

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+	function navbar(){
+		let navbar = document.getElementById('navbar');
+		let logo = document.getElementById('logo');
+		let number = document.querySelector("div.up h3[class='number']");
+		let location = document.querySelector("div.up span[class='location']");
+		let slogan = document.querySelector("div.down h3[class='slogan']");
+		logo.style.width = '15%';
+		navbar.style.position = "sticky";
+		navbar.style.top = 0;
+		navbar.style.fontSize = '1em';
+		navbar.style.flexDirection = 'row';
+		navbar.style.padding = 0;
+		number.style.display = 'none';
+		location.style.display = 'none';
+		slogan.style.display = 'none';
+	}
+
+	window.addEventListener("scroll", navbar);
+
